6d463871148c2d7386a187375a7287168010a36b
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-07 02:23:56 +0900

    Link를 통해 url 이동하는 방법

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 //파일명과 컴포넌트는 상관없음.
+
+import NavBar from '../components/NavBar';
+
 //파일명으로 Routing이 일어난다.
 export default function Home() {
-  return 'hi';
+  return (
+    <div>
+      <NavBar />
+      <h1>Home</h1>
+    </div>
+  );
 }
