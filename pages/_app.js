611ead9a94593fd66b1c5cfc4805fd80252057d4
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-07 03:13:11 +0900

    정리

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
-//next.js가 컴포넌트를 렌더링하기 전에 해당 App()안의 component로 해당 컴포넌트를 전달한다.
-
 import NavBar from '../components/NavBar';
-import '../styles/global.css';
+import '../styles/globals.css';
+//next.js가 컴포넌트를 렌더링하기 전에 해당 App()안의 component로 해당 컴포넌트를 전달한다.
 // _app.js파일만 css파일을 import 해서 사용가능.
-// 다른 파일들은 module.css 파일을 이용해야한다.
+// 다른 파일들은 module.css 파일 or styled jsx를 이용해야한다.
 export default function App({ Component, pageProps }) {
   return (
     <div>
