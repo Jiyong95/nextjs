bbc2be2233dfea60bae90ba72258b43b8fa31b2d
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-07 02:41:43 +0900

    styled jsx 사용법

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,24 +9,23 @@ export default function NavBar() {
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === '/' && styles.active
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === '/' && 'active'}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === '/about' && styles.active,
-          ].join(' ')}
-        >
-          About
-        </a>
+        <a className={router.pathname === '/about' && 'active'}>About</a>
       </Link>
+      {/* styled jsx */}
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text=decoration: none;
+        }
+        .active {
+          color: blue;
+        }
+      `}</style>
     </nav>
   );
 }
