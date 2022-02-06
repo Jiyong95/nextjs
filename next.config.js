e293831e8e50fcaa1b684768533a151c1b76db59
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-07 04:21:04 +0900

    redirect, rewrite 설정

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  // redirect 설정
+  async redirects() {
+    return [
+      // 여러개 만들 수 있다. 객체 형식으로
+      {
+        // /source url 접속시
+        source: '/old-url/:path*',
+        // destination 으로 redirect
+        destination: '/new-url/:path*',
+        permanent: false,
+      },
+    ];
+  },
+  // rewrites는 source는 그대로지만 destination으로 접속한다.
+  // => url을 노출시키지 않을 수 있다(보안)
+  // redirect는 url 자체를 바꿔서 접속해주기 때문에 url이 노출된다.
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
