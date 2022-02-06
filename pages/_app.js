d0a6f28e4c1a2a9fb2d1f113889756c9c287d3cd
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-07 03:25:23 +0900

    Layout, children 이용방법

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,12 @@
-import NavBar from '../components/NavBar';
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 //next.js가 컴포넌트를 렌더링하기 전에 해당 App()안의 component로 해당 컴포넌트를 전달한다.
 // _app.js파일만 css파일을 import 해서 사용가능.
 // 다른 파일들은 module.css 파일 or styled jsx를 이용해야한다.
 export default function App({ Component, pageProps }) {
   return (
-    <div>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
-    </div>
+    </Layout>
   );
 }
