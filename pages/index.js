576fb6f4c9134e15fa03ce0ce17b75c2bc6d1aa6
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-07 04:41:32 +0900

    Server Side Rendering 방법

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch('/api/movies')).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -43,3 +34,35 @@ export default function Home() {
     </div>
   );
 }
+
+// server (side)에서만 작동하는 함수.
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch('http://localhost:3000/api/movies')
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
+// data를 Server에서 받아와서 렌더링하기 때문에 {!results && <h4>Loading...</h4>} 실행될 일이 없어진다.
+// data를 다 받아온 후에 렌더링 하고 싶은 경우에 사용.
+
+/*
+_app.js
+
+ export default function App({ Component, pageProps }) {
+   return (
+     <Layout>
+       <Component {...pageProps} />
+     </Layout>
+   );
+ }
+
+
+Component <= Home()
+pageProps <= getServerSideProps() = {props:{results}}가 전달된다.
+
+즉, Home으로 getServerSideProps()가 전달되어 사용하는 방식.
+*/
